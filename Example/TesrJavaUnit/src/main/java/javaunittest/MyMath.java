930c2a76a511554eee9516396978cb4bb35bc0ff
kolchenkoav <kolchenkoav@gmail.com>
2022-07-13 12:18:35 +0300

    1.07.2022 @ParameterizedTest @CsvSource @CsvFileSource

--- Example/TesrJavaUnit/src/main/java/javaunittest/MyMath.java
+++ Example/TesrJavaUnit/src/main/java/javaunittest/MyMath.java
@@ -8,4 +8,8 @@ public class MyMath {
     public static int div(int a, int b) {
         return a / b;
     }
+
+    public static  int aPlus4(int a) {
+        return a + 4;
+    }
 }
--- Example/TesrJavaUnit/src/test/java/javaunittest/MyMathTest.java
+++ Example/TesrJavaUnit/src/test/java/javaunittest/MyMathTest.java
@@ -1,9 +1,8 @@
 package javaunittest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,4 +32,38 @@ class MyMathTest {
     void div() {
         assertEquals(MyMath.div(12, 3), 4);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12"})
+    //@NullSource
+   // @EmptySource
+    void addParam(int number) {
+        assertEquals(MyMath.add(number, 8), 20);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3",                             //    ( { " 'value1', 'value2', 'result' ", "..." } ) if String
+               "12, 8, 20",
+               "-40, -40, -80",
+               "0, -123456789, -123456789"})
+    void addParam(int value1, int value2, int result) {
+        assertEquals(MyMath.add(value1, value2), result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({    "1, 5",
+                    "10, 14",
+                    "8, 12"})
+    void testInt(int input, int otput) {
+        assertEquals(MyMath.aPlus4( input ), otput);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test     a div b = result   from file  \"/000.csv\"")
+    @CsvFileSource(resources = "/000.csv", numLinesToSkip = 1)     // "/000.csv"
+    public void testDiv(int value1, int value2, int result) {
+        assertEquals(MyMath.div(value1, value2), result);
+    }
+
+
 }
